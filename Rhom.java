c51eb9be81656d8a9fa81543ce76c8cd1c3d79f1
Tharak27 <tharakarame@gmail.com>
2024-12-08 10:09:25 +0530

    Add files via upload

---
+++ Rhom.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Rhom{
+    public static void main(String[] args)
+    {
+        int n=5;
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i+j==n/2 || j-i==n/2 || i-j==n/2 || i+j==n+n/2)
+                {
+                    System.out.print("*"+" ");
+                }
+                else{
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
